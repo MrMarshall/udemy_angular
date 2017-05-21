4317245f50e2fb2bfc05f544a59be341cb4def86
MrMarshall <marcel.renders@gmail.com>
2017-05-21 15:25:36 +0200

    Issue not fixed. Added error handling

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ var schema = new Schema({
   lastName: {type: String, required: true},
   password: {type: String, required: true},
   email: {type: String, required: true, unique: true}
-  //messages: [{type: Schema.types.ObjectId, ref: 'Message'}]
+  messages: [{type: Schema.types.ObjectId, ref: 'Message'}]
 });
 
 schema.plugin(mongooseUniqueValidator);
