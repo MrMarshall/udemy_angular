dade484bfa3ccb0beb48c8fc0f4805e326e9f614
MrMarshall <marcel.renders@gmail.com>
2017-05-26 07:33:23 +0200

    Fix msg service

--- routes/app.js
+++ routes/app.js
@@ -1,33 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error!');
-        }
-        if (doc) {
-          res.render('node', {
-            email: doc.email,
-            firstName: doc.firstName
-          });
-        } else {
-          res.render('node', {email: 'No email address inserted yet'});
-        }
-    });
-});
-
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Marcel',
-        lastName: 'Renders',
-        password: 'badmuts',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
+  res.render('index');
 });
 
 module.exports = router;
