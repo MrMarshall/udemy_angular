669ba84304efbcf6a04efc84ced18c0ad94eae6b
MrMarshall <marcel.renders@gmail.com>
2017-07-03 06:56:03 +0200

    Old uncommitted changes

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
@@ -11,8 +12,9 @@ import { Message } from "./message.model";
 export class MessageInputComponent {
   constructor(private MessageService: MessageService) {}
 
-  onSave(value: string) {
-    const message = new Message(value, 'Marcel');
+  onSubmit(form: NgForm) {
+    const message = new Message(form.value.content, 'Marcel');
     this.MessageService.addMessage(message);
+    form.resetForm();
   }
 }
