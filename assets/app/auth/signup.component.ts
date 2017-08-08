fd27eadb01058881714a7ec0836e935fe76b1218
MrMarshall <marcel.renders@gmail.com>
2017-08-08 08:12:39 +0200

    Trying to fix forms model.

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,10 +1,59 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
-  template: './signup.component.html'
+  templateUrl: './signup.component.html'
 })
 
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  testForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
+  onSubmit() {
+    console.log(this.testForm);
+    //this.myForm.reset();
+  };
+
+  ngOnInit() {
+    this.testForm = this.formBuilder.group({
+    firstname: [null]
+    //this.myForm = new FormGroup({
+    //firstName: new FormControl(null, Validators.required)
+    /*
+    lastName: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl()
+    */
+});
+}}
+
+
+/*
+//Max:
+ngOnInit() {
+  this.myForm = new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+      ]),
+      password: new FormControl(null, Validators.required)
+  });
 }
+*/
+/*
+//Me: original
+ngOnInit() {
+  this.myForm = new FormGroup({
+    firstName: new FormControl(null, Validators.required),
+    lastName: new FormControl(null, Validators.required),
+    //email: new FormControl(null, [
+      //Validators.required//,
+      //Validators.pattern("/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/")
+    //]),
+    password: new FormControl(null, Validators.required)
+  });
+}
+*/
