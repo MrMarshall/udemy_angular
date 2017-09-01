6cd9992db3276f257d7796d15b3232fa625ef6c2
MrMarshall <marcel.renders@gmail.com>
2017-09-01 07:18:50 +0200

    Original

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,59 +1,27 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html'
+    selector: 'app-signup',
+    templateUrl: './signup.component.html'
 })
-
 export class SignupComponent implements OnInit {
-  testForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {}
+    myForm: FormGroup;
 
-  onSubmit() {
-    console.log(this.testForm);
-    //this.myForm.reset();
-  };
+    onSubmit(myForm) {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
 
-  ngOnInit() {
-    this.testForm = this.formBuilder.group({
-    firstname: [null]
-    //this.myForm = new FormGroup({
-    //firstName: new FormControl(null, Validators.required)
-    /*
-    lastName: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl()
-    */
-});
-}}
-
-
-/*
-//Max:
-ngOnInit() {
-  this.myForm = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null, Validators.required),
-      email: new FormControl(null, [
-          Validators.required,
-          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-      ]),
-      password: new FormControl(null, Validators.required)
-  });
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required)
+        });
+    }
 }
-*/
-/*
-//Me: original
-ngOnInit() {
-  this.myForm = new FormGroup({
-    firstName: new FormControl(null, Validators.required),
-    lastName: new FormControl(null, Validators.required),
-    //email: new FormControl(null, [
-      //Validators.required//,
-      //Validators.pattern("/^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/")
-    //]),
-    password: new FormControl(null, Validators.required)
-  });
-}
-*/
