0c1b0017409ce1e7116333d277f364d8ec5e6b57
MrMarshall <marcel.renders@gmail.com>
2017-09-19 07:54:19 +0200

    Update and delete messages.

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 import { MessageService } from "./message.service";
@@ -9,16 +9,40 @@ import { Message } from "./message.model";
   templateUrl: './message-input.component.html',
 })
 
-export class MessageInputComponent {
-  constructor(private MessageService: MessageService) {}
+export class MessageInputComponent implements OnInit {
+  message: Message;
+
+  constructor(private messageService: MessageService) {}
 
   onSubmit(form: NgForm) {
-    const message = new Message(form.value.content, 'Marcel');
-    this.MessageService.addMessage(message)
-      .subscribe(
-        data => console.log(data),
-        error => console.error(error)
-      );
+    if (this.message) {
+      // Edit
+      this.message.content = form.value.content;
+      this.messageService.updateMessage(this.message)
+        .subscribe(
+          result => console.log(result)
+        );
+      this.message = null;
+    } else {
+      //Create
+      const message = new Message(form.value.content, 'Marcel');
+      this.messageService.addMessage(message)
+        .subscribe(
+          data => console.log(data),
+          error => console.error(error)
+        );
+    }
     form.resetForm();
+  }
+
+  onClear(form: NgForm) {
+    this.message = null;
+    form.resetForm();
+  }
+
+  ngOnInit() {
+    this.messageService.messageIsEdit.subscribe(
+      (message: Message) => this.message = message
+    );
   }
 }
